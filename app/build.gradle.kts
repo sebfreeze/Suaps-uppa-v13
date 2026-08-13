9d7d1849c2e9f02996d08c8e825b4881905870b6
sebfreeze <sfrisou@gmail.com>
2026-08-13 22:41:30 +0200

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "fr.univpau.suaps"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "fr.univpau.suaps"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 14
+        versionName = "14.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+}
