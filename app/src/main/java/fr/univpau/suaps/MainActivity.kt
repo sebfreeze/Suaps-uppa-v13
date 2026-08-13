b501f54abb80229477b256386e4f9b1e0f93359d
sebfreeze <sfrisou@gmail.com>
2026-08-13 22:57:05 +0200

    Update MainActivity.kt

--- app/src/main/java/fr/univpau/suaps/MainActivity.kt
+++ app/src/main/java/fr/univpau/suaps/MainActivity.kt
@@ -3,14 +3,17 @@ package fr.univpau.suaps
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val webView = WebView(this)
+        webView = WebView(this)
 
         webView.webViewClient = WebViewClient()
         webView.settings.javaScriptEnabled = true
@@ -19,15 +22,18 @@ class MainActivity : AppCompatActivity() {
         webView.loadUrl("https://suaps-uppa-v13.onrender.com")
 
         setContentView(webView)
-    }
 
-    override fun onBackPressed() {
-        val webView = findViewById<WebView>(android.R.id.content)
-
-        if (webView.canGoBack()) {
-            webView.goBack()
-        } else {
-            super.onBackPressed()
-        }
+        onBackPressedDispatcher.addCallback(
+            this,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if (webView.canGoBack()) {
+                        webView.goBack()
+                    } else {
+                        finish()
+                    }
+                }
+            }
+        )
     }
 }
